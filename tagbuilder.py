b9c3320bb47d20fa60709e7e25851d410ccf0e18
pydave <pydave@gmail.com>
2011-11-20 21:13:23 -0800

    Refactor index builder into smaller functions

--- tagbuilder.py
+++ tagbuilder.py
@@ -92,6 +92,7 @@ class Builder(object):
     def _build_indexes(self, find_args):
         self._build_indexes_impl(find_args, self._root, self._other_set)
         self._build_indexes_impl(find_args, self._working_root, self._working_set)
+        # TODO: top-level cscope should have all files in its fileindex
 
         # We only want the lookupfile database for the top-level
         # TODO: separate the construction and only build them as needed
@@ -103,7 +104,7 @@ class Builder(object):
     def _build_indexes_impl(self, find_args, root, directory_set):
         '''
         Using the input find args (specific to each filetype), builds file
-        lists for the working set and other set.
+        lists for the current set.
 
         Builds a file listing in the format used by lookup file.
         Sorts and properly creates the lookup file index.
@@ -112,15 +113,37 @@ class Builder(object):
         temp_tags = path.join(root, 'temp.tags')
         temp_tags = path.abspath(temp_tags)
 
+        self._build_temp_list(find_args, root, directory_set, temp_tags)
+        self._build_lookup_index(root, temp_tags)
+        self._build_file_index(root, temp_tags)
+
+        os.remove(temp_tags)
+
+    def _build_temp_list(self, find_args, root, directory_set, output_fname):
+        '''
+        Using the input find args (specific to each filetype), builds the basic
+        file list. This file should be removed when we're done: it's not used
+        by any tools.
+
+        Builds a file listing in the format used by lookup file.
+        '''
         # build basic file list
         args = [BIN.find]
         args += directory_set
         args += find_args
         subprocess.call(args,
-                        stdout=open(temp_tags, 'w'),
+                        stdout=open(output_fname, 'w'),
                         close_fds=True,
                         cwd=root)
 
+    def _build_lookup_index(self, root, intermediate_fname):
+        '''
+        Using the input intermediate file (the file list), builds the
+        lookupfile index.
+
+        intermediate_fname: a file list in the format used by lookup file.
+        output: Sorts and properly creates the lookup file index.
+        '''
         # build lookup file index
         lookup_file = self._open_lookupfile_for_path(root)
         lookup_file.write('!_TAG_FILE_SORTED	2	/2=foldcase/\n')
@@ -128,21 +151,29 @@ class Builder(object):
 
         args = [BIN.sort, '-f']
         subprocess.call(args,
-                        stdin=open(temp_tags, 'r'),
+                        stdin=open(intermediate_fname, 'r'),
                         stdout=lookup_file,
                         close_fds=True,
                         cwd=root)
 
+    def _build_file_index(self, root, intermediate_fname):
+        '''
+        Using the input intermediate file (the file list), builds the
+        file index.
+
+        intermediate_fname: a file list in the tag format (used by lookup
+        file).
+        output: Strips extra stuff, sorts, and builds the cscope filelist.
+        '''
         # build file index
         filelist_file = self._open_filelist_for_path(root)
         args = [BIN.cut, '-f2']
         subprocess.call(args,
-                        stdin=open(temp_tags, 'r'),
+                        stdin=open(intermediate_fname, 'r'),
                         stdout=filelist_file,
                         close_fds=True,
                         cwd=root)
 
-        os.remove(temp_tags)
 
     def _open_lookupfile_for_path(self, root):
         return self._open_file_for_path('filenametags', root)
